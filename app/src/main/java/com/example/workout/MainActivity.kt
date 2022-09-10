7d338c1b5f2c2d58089e67f3ae12ed8837de853d
VietNguyen <viet7896@gmail.com>
2022-09-11 00:51:43 +0700

    Add Start Screen

--- app/src/main/java/com/example/workout/MainActivity.kt
+++ app/src/main/java/com/example/workout/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.workout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.workout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        binding.flStart.setOnClickListener{
+            Toast.makeText(this,"Start",Toast.LENGTH_SHORT).show()
+        }
     }
 }
