42a273dcd82da60308212f19955e343a24141748
VietNguyen <viet7896@gmail.com>
2022-09-11 21:59:57 +0700

    Setup CountDown Timer

--- app/src/main/java/com/example/workout/ExerciseActivity.kt
+++ app/src/main/java/com/example/workout/ExerciseActivity.kt
@@ -2,12 +2,16 @@ package com.example.workout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 import com.example.workout.databinding.ActivityExerciseBinding
 import com.example.workout.databinding.ActivityMainBinding
 
 class ExerciseActivity : AppCompatActivity() {
     private lateinit var binding: ActivityExerciseBinding
+    private var restTimer: CountDownTimer? = null
+    private var restProgress = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityExerciseBinding.inflate(layoutInflater)
@@ -21,9 +25,40 @@ class ExerciseActivity : AppCompatActivity() {
         binding.toolbarExercise.setNavigationOnClickListener{
             onBackPressed()
         }
+        setupRestView()
     }
+    private fun setupRestView(){
+        if(restTimer != null) {
+            restTimer?.cancel()
+            restProgress = 0
+        }
+        setRestProgressBar()
+    }
+private fun setRestProgressBar(){
+    binding.progressBar.progress = restProgress
+    restTimer = object: CountDownTimer(10000,1000){
+        override fun onTick(p0: Long) {
+           restProgress++
+            binding.progressBar.progress = 10 - restProgress
+            binding.tvTimer.text = (10 - restProgress).toString()
+        }
+
+        override fun onFinish() {
+           Toast.makeText(this@ExerciseActivity,"Start",Toast.LENGTH_SHORT).show()
+        }
+
+    }.start()
+}
+
+    override fun onDestroy() {
+        super.onDestroy()
+        if(restTimer != null) {
+            restTimer?.cancel()
+            restProgress = 0
+        }
 
 
+    }
 
 
 }
